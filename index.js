c3a96e191e78fa2df016c875b5dc67c3156268a9
ipetrash <gil9red@gmail.com>
2016-02-11 16:01:02 +0500

    example using about:config

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ var buttons = require('sdk/ui/button/action');
 var tabs = require("sdk/tabs");
 var pageMod = require("sdk/page-mod");
 
+// http://stackoverflow.com/questions/3796084/about-config-preferences-and-js
+let { Cc, Ci } = require('chrome');
+
+// Get the "accessibility." branch
+var prefs = Cc["@mozilla.org/preferences-service;1"].getService(Ci.nsIPrefService).getBranch("accessibility.");
+
+// prefs is an nsIPrefBranch.
+// Look in the above section for examples of getting one.
+var value = prefs.getBoolPref("typeaheadfind"); // get a pref (accessibility.typeaheadfind)
+d('value=' + value)
+//prefs.setBoolPref("typeaheadfind", !value); // set a pref (accessibility.typeaheadfind)
+
 //const fileIO = require("sdk/io/file");
 //
 //let path = "E:/";
