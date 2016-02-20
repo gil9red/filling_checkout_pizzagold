6ac5e2293c994208b9c397e00981f5f67c64592f
Ilya Petrash <ilya.petrash@inbox.ru>
2016-02-20 20:20:29 +0600

    Удачное заполнение полей на странице, взятых из полей настроек плагина

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var pageMod = require("sdk/page-mod");
 var URL = 'http://www.pizzagold.ru/basket';
 var INCLUDES = ["http://www.pizzagold.ru/basket*", "https://www.pizzagold.ru/basket*"];
 
-URL = 'https://github.com/settings/profile';
+//URL = 'https://github.com/settings/profile';
 
 if (DEBUG) {
     d("Open debug tab");
@@ -25,11 +25,11 @@ if (DEBUG) {
 //    tabs.open('http://torrent.mgn.ru');
 //    tabs.open('http://www.pizzagold.ru/basket/');
 //    tabs.open('http://torrent.mgn.ru/viewtopic.php?t=78458');
-//    tabs.open('http://www.pizzagold.ru/basket');
-    tabs.open('https://github.com/settings/profile');
+    tabs.open('http://www.pizzagold.ru/basket');
+//    tabs.open('https://github.com/settings/profile');
 //    tabs.open('https://github.com/join?return_to=https%3A%2F%2Fgithub.com%2Fsettings%2Fprofile&source=login');
 
-//    tabs[0].close();
+    tabs[0].close();
 }
 
 // Ссылка на кнопку плагина
@@ -45,32 +45,41 @@ var order_personal_pager = prefs.my_pager;
 var order_personal_mailbox = prefs.my_mailbox;
 var order_personal_notes = prefs.my_notes;
 
+d("order_name: " + order_name);
+d("order_personal_phone: " + order_personal_phone);
+d("order_email: " + order_email);
+d("order_personal_street: " + order_personal_street);
+d("order_personal_pager: " + order_personal_pager);
+d("order_personal_mailbox: " + order_personal_mailbox);
+d("order_personal_notes: " + order_personal_notes);
+
 var contentScript = "function set(id, value) {" +
 "    var el = document.getElementById(id);" +
 "    if (el != null) el.value = value;" +
 "}\n" +
 
 "// Имя\n" +
-"set('ORDER_NAME', <order_name>);\n" +
+"set('ORDER_NAME', '<order_name>');\n" +
 
 "// Телефон (обязательный)\n" +
-"set('ORDER_PERSONAL_PHONE', <order_personal_phone>);\n" +
+"set('ORDER_PERSONAL_PHONE', '<order_personal_phone>');\n" +
 
 "// Почта\n" +
-"set('ORDER_EMAIL', <order_email>);\n" +
+"set('ORDER_EMAIL', '<order_email>');\n" +
 
 "// Улица\n" +
-"set('ORDER_PERSONAL_STREET', <order_personal_street>);\n" +
+"set('ORDER_PERSONAL_STREET', '<order_personal_street>');\n" +
 
 "// Дом\n" +
-"set('ORDER_PERSONAL_PAGER', <order_personal_pager>);\n" +
+"set('ORDER_PERSONAL_PAGER', '<order_personal_pager>');\n" +
 
 "// Квартира\n" +
-"set('ORDER_PERSONAL_MAILBOX', <order_personal_mailbox>);\n" +
+"set('ORDER_PERSONAL_MAILBOX', '<order_personal_mailbox>');\n" +
 
 "// Комментарий к заказу\n" +
-"set('ORDER_PERSONAL_NOTES', <order_personal_notes>);\n"
+"set('ORDER_PERSONAL_NOTES', '<order_personal_notes>');\n";
 
+contentScript = contentScript
 .replace("<order_name>", order_name)
 .replace("<order_personal_phone>", order_personal_phone)
 .replace("<order_email>", order_email)
@@ -79,7 +88,12 @@ var contentScript = "function set(id, value) {" +
 .replace("<order_personal_mailbox>", order_personal_mailbox)
 .replace("<order_personal_notes>", order_personal_notes);
 
+
+// TODO: ставить галочку напротив: Я соглашаюсь с условиями оплаты и доставки.
+//<input value="Y" name="OFFER" type="checkbox">
+
 d(contentScript);
+
 
 function createButton() {
     d("Start create button");
